49d91cdd92f2b77748d060adb2fe060895371a38
James Kinyua <jameskinyua590@gmail.com>
2017-06-13 19:52:04 +0300

    Add validation to EntryForm and UserForm classes

--- models/forms.py
+++ models/forms.py
@@ -1,13 +1,14 @@
 import wtforms
+from wtforms.validators import DataRequired
 
 class EntryForm(wtforms.Form):
-    title = wtforms.StringField('Entry Name')
+    title = wtforms.StringField('Entry Name', validators=[DataRequired()])
     status = wtforms.SelectField(
         'Entry status',
         choices=((True, 'Done'),
                  (False, 'Not Done')))
                  
 class UserForm(wtforms.Form):
-    first_name = wtforms.StringField('First Name')
-    last_name = wtforms.StringField('Last Name')
-    password = wtforms.PasswordField('Password')
+    first_name = wtforms.StringField('First Name', validators=[DataRequired()])
+    last_name = wtforms.StringField('Last Name', validators=[DataRequired()])
+    password = wtforms.PasswordField('Password', validators=[DataRequired()])
