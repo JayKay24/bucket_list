e274b1664a8814e0ab4cc3008f05bdeb036dbd46
James Kinyua <jameskinyua590@gmail.com>
2017-06-13 19:53:37 +0300

    Add docstrings to classes UserForm and EntryForm

--- models/forms.py
+++ models/forms.py
@@ -2,6 +2,9 @@ import wtforms
 from wtforms.validators import DataRequired
 
 class EntryForm(wtforms.Form):
+    """
+    Class to generate an entry form.
+    """
     title = wtforms.StringField('Entry Name', validators=[DataRequired()])
     status = wtforms.SelectField(
         'Entry status',
@@ -9,6 +12,9 @@ class EntryForm(wtforms.Form):
                  (False, 'Not Done')))
                  
 class UserForm(wtforms.Form):
+    """
+    Class to generate a user form.
+    """
     first_name = wtforms.StringField('First Name', validators=[DataRequired()])
     last_name = wtforms.StringField('Last Name', validators=[DataRequired()])
     password = wtforms.PasswordField('Password', validators=[DataRequired()])
