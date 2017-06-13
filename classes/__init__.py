9832fc64b43bec30824a09dc7114c9b4af72988f
James Kinyua <jameskinyua590@gmail.com>
2017-06-13 19:11:59 +0300

    Create class User inside classes package

---
+++ classes/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 13 19:00:28 2017
+
+@author: Geoffrey.ngae
+"""
+
---
+++ classes/user.py
@@ -0,0 +1,9 @@
+class User:
+    """
+    Class to instantiate User objects.
+    """
+    def __init__(self, fname, lname):
+        self.first_name = fname
+        self.last_name = lname
+        
+        
